a383e7fa6eb0b106a58fa4d8392c59eaa50eb2f6
Arfaz Chougule <arfaz.chougule@codinova.com>
2024-03-28 14:31:54 +0530

    update response

--- src/app/api/features/route.ts
+++ src/app/api/features/route.ts
@@ -2,10 +2,10 @@ import { features } from "@/constants/seeds";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return new NextResponse(JSON.stringify({ error: "failed" }), {
-    status: 400,
-  });
-  // return new NextResponse(JSON.stringify({ features }), {
-  //   status: 200,
+  // return new NextResponse(JSON.stringify({ error: "failed" }), {
+  //   status: 400,
   // });
+  return new NextResponse(JSON.stringify({ features }), {
+    status: 200,
+  });
 }
